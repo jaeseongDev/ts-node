793314db948c114cecb3c179a56d32ec8c5372e6
박재성 <jaeseong@bagjaeseong-ui-MacBookAir.local>
2021-12-20 09:32:29 +0900

    타입스크립트 라우팅 코드 추가

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-import express from 'express'
+import express from 'express';
+import config from 'config';
+import log from './logger';
+import connect from './db/connect';
+import routes from './routes';
 
-const app = express()
+const port = config.get("port") as number;
+const host = config.get("host") as string;
 
-app.get('/', (req, res, next) => {
-  res.send('Hello')
-})
+const app = express();
 
-app.listen(8080, () => {
-    console.log('Server running')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+  log.info(`Server listening at http://${host}:${port}`);
+
+  connect();
+
+  routes(app);
 })
---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Express, Request, Response } from 'express'
+export default function(app: Express) {
+  app.get('/healthcheck', (req:Request, res:Response) => res.sendStatus(200));
+  
+// Register user
+  
+
+// Login
+
+// Get the user's sessions
+
+// 
+}
